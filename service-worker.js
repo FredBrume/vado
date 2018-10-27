035032d4c5c9887083e1ef428bf72e1376428419
FredBrume <FredBrume@users.github.com>
2018-10-27 19:00:19 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/vado/precache-manifest.99eb779c2b2829df780e8a295fbfc86e.js"
+  "/vado/precache-manifest.17fb573d652d380dd2fb0f3403ed7376.js"
 );
 
 workbox.clientsClaim();
